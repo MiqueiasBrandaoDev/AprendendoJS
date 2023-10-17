65a81fb60494fc80a1d0c29c032ee98e095c732b
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-10-17 05:22:04 -0300

    17.10

--- login/js/script.js
+++ login/js/script.js
@@ -1,8 +1,18 @@
 function carregando(){
     
-    let sppiner = document.querySelector("#loading")
+    document.querySelector("#loading").classList.remove("invisible")
     document.querySelector("#btn-login").classList.remove("btn-primary")
     document.querySelector("#btn-login").classList.add("btn-warning")
-    sppiner.classList.remove("invisible")
+    
+    setTimeout(() => {
+        
+        document.querySelector("#btn-login").classList.remove("btn-warning")
+        document.querySelector("#btn-login").classList.add("btn-success")
+        document.querySelector("#loading").classList.remove("spinner-border-sm")
+        document.querySelector("#loading").classList.remove("spinner-border")
+        document.querySelector("#loading").classList.add("bi")
+        document.querySelector("#loading").classList.add("bi-check-circle-fill")
 
+    }, 2000);
+ 
 }
