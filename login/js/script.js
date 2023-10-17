f7bca42641a208eb39b8bcc1f77528dab95eeab9
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-10-16 21:31:37 -0300

    16.10

---
+++ login/js/script.js
@@ -0,0 +1,8 @@
+function carregando(){
+    
+    let sppiner = document.querySelector("#loading")
+    document.querySelector("#btn-login").classList.remove("btn-primary")
+    document.querySelector("#btn-login").classList.add("btn-warning")
+    sppiner.classList.remove("invisible")
+
+}
