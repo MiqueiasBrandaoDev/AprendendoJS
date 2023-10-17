bc9dad51bb1f6c2080e00b92f031264ebb13b858
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-10-17 05:29:30 -0300

    16.10

--- login/js/script.js
+++ login/js/script.js
@@ -8,11 +8,12 @@ function carregando(){
         
         document.querySelector("#btn-login").classList.remove("btn-warning")
         document.querySelector("#btn-login").classList.add("btn-success")
+        document.querySelector("#btn-login").textContent = "LOGIN SUCEDIDO"
         document.querySelector("#loading").classList.remove("spinner-border-sm")
         document.querySelector("#loading").classList.remove("spinner-border")
         document.querySelector("#loading").classList.add("bi")
         document.querySelector("#loading").classList.add("bi-check-circle-fill")
-
+  
     }, 2000);
  
 }
