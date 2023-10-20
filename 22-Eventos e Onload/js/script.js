b49110f6a7fbc80c1563be7280fdf1f1b1437a32
miqueiasbrandao <106256279+miqueiasbrandao@users.noreply.github.com>
2023-10-20 06:18:56 -0300

    Estudo de Eventos 20.10

--- 22-Eventos e Onload/js/script.js
+++ 22-Eventos e Onload/js/script.js
@@ -1,6 +1,35 @@
-//SÓ EXECUTA QUANDO A PAGE ESTÁ TOTALMENTE CARREGADA
-window.onload = function(){
-    console.log("Página 100% carregada")
+let seletor = {
+    onload: function(){
+        //SÓ EXECUTA QUANDO A PAGE ESTÁ TOTALMENTE CARREGADA
+        window.onload = function(){
+            console.log("Página 100% carregada")
+        }
+        console.log("Página 100% carregada-fora do onload")
+    },
+
+    addEventListener: function(){
+        //CAPTURA UM EVENTO DE CLICK DO USUÁRIO PARA DETERMINADO ELEMENTO
+        let bodyEvent = document.querySelector("#btn")
+
+        //MUDANDO COR DO BOTAO APOS A CAPTURA DO CLICK
+        bodyEvent.addEventListener("click", function(){//eventos após o click
+            console.log("Clicou no botão")
+            this.style.backgroundColor = "blue"
+        })
+
+        //REMOVENDO SUBTITLE SE CLICAR 2 VEZES NO TITULO
+        let titleElement = document.querySelector("#title")
+        titleElement.addEventListener("dblclick", function(){
+            document.querySelector("#subtitle").style.display = "none"
+        })
+
+        //ADICIONANDO MENSAGEM PRA - NÃO SAIR
+        let tagHtml = document.querySelector("#title")
+        tagHtml.addEventListener("mouseover", function(){
+            document.querySelector("#title").innerHTML = "Não saia, temos um desconto de 90% para você!"
+        })
+    }
+
 }
 
-console.log("Página 100% carregada-fora do onload")
+seletor.addEventListener()
