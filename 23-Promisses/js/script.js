7bc2f20bcb1debc3e0a334afba9cdc4537ac339e
MiqueiasBrandaoDev <miqueiasbrandaogyn@gmail.com>
2023-10-23 12:02:50 -0300

    alteração da promise

--- 23-Promisses/js/script.js
+++ 23-Promisses/js/script.js
@@ -18,5 +18,4 @@ const minhaPromisse = new Promise((resolve, reject) => {
 //recebendo os dados para tratar
 minhaPromisse
 .then((data) => {return data.toLowerCase()})//then resolve - retorna o dado e trata
-.then((stringModificada) => {console.log(stringModificada)})//encadeamento de thens - usa os dados do then anterior
 .catch((err) => {console.log("Aconteceu um erro - " + err)})//then reject
